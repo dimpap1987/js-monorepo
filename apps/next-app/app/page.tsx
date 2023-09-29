efe90e2a7e08593e19c32802c96945d20280ec54
Dpap <dim.papoutsis.1987@gmail.com>
2023-09-29 18:29:04 +0300

    Create a loading component #2

--- apps/next-app/app/page.tsx
+++ apps/next-app/app/page.tsx
@@ -1,7 +1,10 @@
+import { Loader } from '@js-monorepo/loader'
+
 export default async function Index() {
   return (
     <main>
       <h1> Hello everyone...</h1>
+      <Loader message="Waiting for transcation" show={false}></Loader>
     </main>
   )
 }
