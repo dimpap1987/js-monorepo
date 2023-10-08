25a3d1f49cb8715197ede111389c021226323130
Dpap <dim.papoutsis.1987@gmail.com>
2023-10-08 22:19:01 +0300

    Button component (#6)

---
+++ libs/shared/ui/button/src/lib/button.tsx
@@ -0,0 +1,65 @@
+import { twMerge } from 'tailwind-merge'
+
+/* eslint-disable-next-line */
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary' | 'danger'
+  size?: 'small' | 'medium' | 'large'
+  className?: string
+  children?: React.ReactNode
+  loading?: boolean
+}
+
+export function ButtonComponent({
+  variant = 'primary',
+  size = 'medium',
+  className,
+  children,
+  loading = false,
+  ...props
+}: ButtonProps) {
+  //disabled classes
+  const disabledStyles = 'bg-gray-300 cursor-not-allowed opacity-50'
+  // base styles
+  const baseStyles = `relative px-12 py-2 rounded font-bold focus:outline-none flex items-center ${
+    loading ? disabledStyles : ''
+  }`
+  // Variant styles
+  const variantStyles = {
+    primary: 'bg-blue-500 text-white hover:bg-blue-600',
+    secondary: 'bg-gray-200 text-black hover:bg-gray-300',
+    danger: 'bg-red-500 text-white hover:bg-red-600',
+    // Add more variants as needed
+  }
+
+  // Size styles
+  const sizeStyles = {
+    small: 'text-sm py-1',
+    medium: 'text-base py-2',
+    large: 'text-lg py-3',
+  }
+
+  // Merge styles using tailwind-merge
+  const buttonClass = twMerge(
+    baseStyles,
+    variantStyles[variant],
+    sizeStyles[size],
+    className // Merge additional classes passed as props
+  )
+
+  const buttonContent = children ?? 'Press button'
+  const loadingContent = loading && (
+    <div
+      className="inline-block absolute left-4 h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent  motion-reduce:animate-[spin_1.5s_linear_infinite] mr-3"
+      role="status"
+    ></div>
+  )
+
+  return (
+    <button className={buttonClass} {...props} disabled={loading}>
+      {loadingContent}
+      {buttonContent}
+    </button>
+  )
+}
+
+export default ButtonComponent
