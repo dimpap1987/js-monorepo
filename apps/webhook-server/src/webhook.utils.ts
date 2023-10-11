24b21da2d6fa786b5700992be2ad22ae0b25947c
dimpap1987 <97888300+dimpap1987@users.noreply.github.com>
2023-10-11 12:45:29 +0300

    Feat/webhookserver (#17)
    Webhook server - github actions

---
+++ apps/webhook-server/src/webhook.utils.ts
@@ -0,0 +1,45 @@
+import crypto from 'crypto'
+import { Request } from 'express'
+import { exec } from 'child_process'
+
+const WEBHOOK_SECRET: string = process.env.WEBHOOK_SECRET || ''
+
+const verifySignature = (req: Request) => {
+  const signature = crypto
+    .createHmac('sha256', WEBHOOK_SECRET)
+    .update(JSON.stringify(req.body))
+    .digest('hex')
+
+  const trusted = Buffer.from(`sha256=${signature}`, 'ascii')
+
+  const headerValue = req.headers['x-hub-signature-256']
+  const untrusted =
+    typeof headerValue === 'string'
+      ? Buffer.from(headerValue, 'ascii')
+      : Buffer.alloc(0)
+
+  return crypto.timingSafeEqual(trusted, untrusted)
+}
+
+const validateWebhookRequest = (req: Request) => {
+  if (!verifySignature(req)) {
+    throw new Error('Unauthorized')
+  }
+}
+
+const executeDockerCompose = (
+  project: string,
+  successCallBack: () => void,
+  errorCallBack: () => void
+) => {
+  exec(
+    `docker compose -f docker-compose.${project}.yml up -d`,
+    (error, _stdout, _stderr) => {
+      if (error) {
+        errorCallBack()
+      }
+      successCallBack()
+    }
+  )
+}
+export { validateWebhookRequest, executeDockerCompose }
