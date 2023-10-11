d51c1ee4c370af7b327d096abdc981708b8fb23f
dimpap1987 <97888300+dimpap1987@users.noreply.github.com>
2023-10-11 12:45:29 +0300

    Feat/webhookserver (#17)
    Webhook server - github actions

---
+++ apps/webhook-server/src/main.ts
@@ -0,0 +1,39 @@
+import express, { Request, Express } from 'express'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import { executeDockerCompose, validateWebhookRequest } from './webhook.utils'
+
+const port = process.env.PORT || 3333
+
+const app: Express = express()
+
+// Morgan logger setup
+app.use(morgan('combined'))
+
+app.use(bodyParser.json())
+
+app.post('/api/:project', (req: Request, res) => {
+  try {
+    validateWebhookRequest(req)
+  } catch (err) {
+    return res.status(401).send({ message: 'Unauthorized' })
+  }
+  const project = req.params.project
+
+  executeDockerCompose(
+    project,
+    () => {
+      return res.send({ message: 'Docker Compose started successfully.' })
+    },
+    () => {
+      return res
+        .status(500)
+        .send({ message: 'Failed to start Docker Compose.' })
+    }
+  )
+})
+
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`)
+})
+server.on('error', console.error)
