779c34cc9a66c688f004a05d92e99f3d41b2bc73
Dpap <dim.papoutsis.1987@gmail.com>
2023-10-20 14:25:21 +0300

    Changes on navbar component & Loader

--- libs/shared/ui/loader/src/lib/loader.tsx
+++ libs/shared/ui/loader/src/lib/loader.tsx
@@ -19,7 +19,81 @@ export function Loader({ message, show }: LoaderProps) {
           role="status"
           className="flex items-center"
         >
-          <div className={`${styles.spinner}`}></div>
+          <svg className={styles.icon} viewBox="0 0 256 256">
+            <line
+              x1="128"
+              y1="32"
+              x2="128"
+              y2="64"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="34"
+            ></line>
+            <line
+              x1="195.9"
+              y1="60.1"
+              x2="173.3"
+              y2="82.7"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="34"
+            ></line>
+            <line
+              x1="224"
+              y1="128"
+              x2="192"
+              y2="128"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="34"
+            ></line>
+            <line
+              x1="195.9"
+              y1="195.9"
+              x2="173.3"
+              y2="173.3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="34"
+            ></line>
+            <line
+              x1="128"
+              y1="224"
+              x2="128"
+              y2="192"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="34"
+            ></line>
+            <line
+              x1="60.1"
+              y1="195.9"
+              x2="82.7"
+              y2="173.3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="34"
+            ></line>
+            <line
+              x1="32"
+              y1="128"
+              x2="64"
+              y2="128"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="34"
+            ></line>
+            <line
+              x1="60.1"
+              y1="60.1"
+              x2="82.7"
+              y2="82.7"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="34"
+            ></line>
+          </svg>
+          <div className="leading-4 font-bold ml-1">Loading...</div>
         </div>
         {message && (
           <p>
