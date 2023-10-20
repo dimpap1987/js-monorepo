fc286d442f3b5c9d016ae810727e014d88965e25
Dpap <dim.papoutsis.1987@gmail.com>
2023-10-20 14:25:21 +0300

    Changes on navbar component & Loader

--- libs/shared/ui/navbar/src/lib/navbar.tsx
+++ libs/shared/ui/navbar/src/lib/navbar.tsx
@@ -31,9 +31,17 @@ export function NavbarComponent({
 }: NavbarProps) {
   //state
   const [isLoginDialog, setLoginDialog] = useState(false)
-  const [isDropdownRefVisible, setIsDropdownRefVisible] = useState(false) // State to show/hide div
-  const dropdownRef = useRef<HTMLDivElement | null>(null)
+  const [
+    isDropdownLoggedOptionsRefVisible,
+    setIsDropdownLoggedOptionsRefVisible,
+  ] = useState(false) // State to show/hide div
+  const dropdownLoggedOptionsRef = useRef<HTMLDivElement | null>(null)
   const dropdownIconRef = useRef<SVGSVGElement | null>(null)
+
+  const [isDropdownMenuRefVisible, setIsDropdownMenuRefVisible] =
+    useState(false) // State to show/hide div
+  const dropdownMenuRef = useRef<HTMLDivElement | null>(null)
+  const dropDownMenuIconRef = useRef<HTMLDivElement | null>(null)
 
   const { logo } = useMemo(() => {
     let logoElement: ReactNode | null = null
@@ -56,11 +64,18 @@ export function NavbarComponent({
     // Click handler to check if clicks are outside of the referenced div
     const handleClickOutside = (event: MouseEvent) => {
       if (
-        !dropdownRef.current?.contains(event.target as Node) &&
+        !dropdownLoggedOptionsRef.current?.contains(event.target as Node) &&
         !dropdownIconRef.current?.contains(event.target as Node) &&
         event.target !== dropdownIconRef.current
       ) {
-        setIsDropdownRefVisible(false)
+        setIsDropdownLoggedOptionsRefVisible(false)
+      }
+      if (
+        !dropdownMenuRef.current?.contains(event.target as Node) &&
+        !dropDownMenuIconRef.current?.contains(event.target as Node) &&
+        event.target !== dropdownMenuRef.current
+      ) {
+        setIsDropdownMenuRefVisible(false)
       }
     }
 
@@ -74,12 +89,10 @@ export function NavbarComponent({
 
   return (
     <nav className="z-40 bg-primary-dark text-white w-screen">
-      <div className={`flex justify-between h-14`}>
+      <div className="flex justify-between h-14">
         <div className="px-5 xl:px-12 py-2 flex w-full items-center">
-          {/* options on the left*/}
           {logo}
 
-          {/* options on the center*/}
           {menuItems && menuItems.length > 0 && (
             <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
               {menuItems.map((item, index) => (
@@ -98,15 +111,14 @@ export function NavbarComponent({
               onClick={() => setLoginDialog((prev) => !prev)}
             ></LoginButtonComponent>
             {/* when logged in */}
-            <label
-              htmlFor="userOptionsToggle"
+            <div
               className="flex items-center hover:text-gray-200 cursor-pointer select-none transition duration-300 ease-in-out hover:scale-125"
+              onClick={() => {
+                setIsDropdownLoggedOptionsRefVisible((prev) => !prev)
+              }}
             >
               <svg
                 ref={dropdownIconRef}
-                onClick={() => {
-                  setIsDropdownRefVisible((prev) => !prev)
-                }}
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6 hover:text-gray-200"
                 fill="none"
@@ -120,14 +132,17 @@ export function NavbarComponent({
                   d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-            </label>
+            </div>
           </div>
         </div>
 
         {/* Dropdown icon */}
-        <label
-          htmlFor="menuToggle"
+        <div
           className="navbar-burger self-center mr-6 md:hidden cursor-pointer select-none py-2 px-4"
+          ref={dropDownMenuIconRef}
+          onClick={() =>
+            setIsDropdownMenuRefVisible(() => !isDropdownMenuRefVisible)
+          }
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -143,45 +158,41 @@ export function NavbarComponent({
               d="M4 6h16M4 12h16M4 18h16"
             />
           </svg>
-        </label>
+        </div>
       </div>
 
-      {/* Hidden input to toggle dropdown options */}
-      <input
-        id="menuToggle"
-        type="checkbox"
-        className={`${styles.menuToggleChecked} hidden absolute select-none`}
-      ></input>
-      {/* Dropdown */}
-      <div
-        className={`hidden absolute w-52 right-0 p-2 shadow-lg bg-primary-dark text-white ${styles.dropdownMenu}`}
-      >
-        {menuItems && menuItems.length > 0 && (
-          <ul className="mx-auto font-semibold font-heading">
-            {menuItems.map((item, index) => (
-              <li
-                key={index}
-                className="text-center py-2 hover:bg-blue-900 w-full flex justify-center"
-              >
-                <NavLink className="py-2 px-4" href={item.href}>
-                  {item.name}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        )}
-        <hr className="my-2" />
-        <LoginButtonComponent
-          onClick={() => setLoginDialog((prev) => !prev)}
-        ></LoginButtonComponent>
-        <LogoutButtonComponent></LogoutButtonComponent>
-      </div>
+      {isDropdownMenuRefVisible && (
+        <div
+          className="absolute w-52 right-0 p-2 shadow-lg bg-primary-dark text-white block md:hidden"
+          ref={dropdownMenuRef}
+        >
+          {menuItems && menuItems.length > 0 && (
+            <ul className="mx-auto font-semibold font-heading">
+              {menuItems.map((item, index) => (
+                <li
+                  key={index}
+                  className="text-center py-2 hover:bg-blue-900 w-full flex justify-center"
+                >
+                  <NavLink className="py-2 px-4" href={item.href}>
+                    {item.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          )}
+          <hr className="my-2" />
+          <LoginButtonComponent
+            onClick={() => setLoginDialog((prev) => !prev)}
+          ></LoginButtonComponent>
+          <LogoutButtonComponent></LogoutButtonComponent>
+        </div>
+      )}
 
       {/* User options hidden input*/}
-      {isDropdownRefVisible && (
+      {isDropdownLoggedOptionsRefVisible && (
         <div
-          ref={dropdownRef}
-          className={`absolute w-44 right-0 p-2 shadow-lg bg-primary-dark text-white z-40 hidden md:block`}
+          ref={dropdownLoggedOptionsRef}
+          className="absolute w-44 right-0 p-2 shadow-lg bg-primary-dark text-white z-40 hidden md:block"
         >
           <LogoutButtonComponent></LogoutButtonComponent>
         </div>
