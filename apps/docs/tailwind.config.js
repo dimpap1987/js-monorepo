b49c0ab4c6377b53862cac1e6aac487d34e69d3d
Dpap <dim.papoutsis.1987@gmail.com>
2024-01-09 02:22:47 +0200

    Docs next app generated for helping with the documentation of the libraries

---
+++ apps/docs/tailwind.config.js
@@ -0,0 +1,14 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind')
+const { join } = require('path')
+
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  // eslint-disable-next-line global-require
+  plugins: [require('@tailwindcss/typography')],
+}
