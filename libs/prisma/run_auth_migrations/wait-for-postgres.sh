68b0b046eb37492df99e22360898444a3d4b81e6
Dpap <dim.papoutsis.1987@gmail.com>
2024-03-04 15:05:32 +0200

    Deployment changes

---
+++ libs/prisma/run_auth_migrations/wait-for-postgres.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+  
+host="$1"
+user="$2"
+auth="$3"
+shift
+shift
+shift
+cmd="$@"
+  
+until PGPASSWORD=$AUTH_DATABASE_PASSWORD psql -h "$host" -U "$user" -d "$auth" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+  
+>&2 echo "Postgres is up - executing command"
+
+exec $cmd
