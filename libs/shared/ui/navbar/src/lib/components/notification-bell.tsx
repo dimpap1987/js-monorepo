cfb7af9ab19117696df6cab6d5d33f6f62f3d159
PAPOUTSIS Dimitris <dimitris.papoutsis@sword-group.com>
2024-05-02 16:02:24 +0300

    Notification component added

--- libs/shared/ui/navbar/src/lib/components/notification-bell.tsx
+++ libs/shared/ui/navbar/src/lib/components/notification-bell.tsx
@@ -1,37 +1,84 @@
 'use client'
 
-import { useState } from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@js-monorepo/dropdown'
+import { Fragment, useState } from 'react'
 import { IoMdNotifications } from 'react-icons/io'
 import { MdNotificationsActive } from 'react-icons/md'
 import './bell.css'
-import { cn } from '@js-monorepo/utils'
 
 type NotificationType = {
+  id: number
   message: string
   isRead: boolean
+  timeAgo: string
 }
 
-const notifcationList: NotificationType[] = [
+const notificationListDummy: NotificationType[] = [
   {
-    message: 'User jim has logged in',
-    isRead: true,
-  },
-  {
-    message: 'User dimpap has logged in',
+    id: 1,
+    message: 'User jim has logged in ❤️',
     isRead: false,
+    timeAgo: '3days ago',
   },
   {
-    message: 'Panatha ole',
-    isRead: true,
-  },
-  {
-    message: 'Panatha ole!!!!!',
+    id: 2,
+    message: 'User dimpap has signed out 👌',
     isRead: false,
+    timeAgo: '2days ago',
+  },
+  {
+    id: 3,
+    message: 'Panatha ole 🤩',
+    isRead: false,
+    timeAgo: '1day ago',
+  },
+  {
+    id: 4,
+    message: 'Panatha ole!!!!! 😁',
+    isRead: false,
+    timeAgo: '1min ago',
+  },
+  {
+    id: 5,
+    message: 'Panatha ole!!!!! 😁',
+    isRead: false,
+    timeAgo: '1min ago',
+  },
+  {
+    id: 6,
+    message: 'Panatha ole!!!!! 😁',
+    isRead: false,
+    timeAgo: '1min ago',
+  },
+  {
+    id: 7,
+    message: 'Panatha ole!!!!! 😁',
+    isRead: true,
+    timeAgo: '1min ago',
+  },
+  {
+    id: 8,
+    message: 'Panatha ole!!!!! 😁',
+    isRead: true,
+    timeAgo: '1min ago',
+  },
+  {
+    id: 9,
+    message: 'Panatha ole!!!!! 😁',
+    isRead: true,
+    timeAgo: '1min ago',
   },
 ]
 
 function DpNotificationBell({
-  notificationList = notifcationList,
+  notificationList = notificationListDummy,
   className,
 }: {
   notificationList?: NotificationType[]
@@ -43,21 +90,55 @@ function DpNotificationBell({
   const isRinging = notificationCount > 0
 
   return (
-    <div className={cn('flex relative cursor-pointer', className)}>
-      {/* label */}
-      {isRinging && (
-        <div className="absolute rounded-full border w-[20px] h-[20px] transform translate-x-4 -translate-y-3 bg-orange-700 border-orange-700 text-white text-sm">
-          {notificationCount}
-        </div>
-      )}
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className={className}>
+        <button className="outline-none">
+          {isRinging && (
+            <div className="absolute rounded-full border w-[20px] h-[20px] transform translate-x-4 -translate-y-3 bg-orange-700 border-orange-700 text-white text-sm">
+              {notificationCount}
+            </div>
+          )}
 
-      {/* Bell */}
-      {isRinging ? (
-        <MdNotificationsActive className="animate-bell text-2xl" />
-      ) : (
-        <IoMdNotifications className="text-2xl" />
-      )}
-    </div>
+          {/* Bell */}
+          {isRinging ? (
+            <MdNotificationsActive className="animate-bell text-2xl" />
+          ) : (
+            <IoMdNotifications className="text-2xl" />
+          )}
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="pl-2 bg-background-secondary mt-3 hidden md:block text-white h-[315px] overflow-y-auto">
+        {notifications &&
+          notifications.length > 0 &&
+          notifications.map((notification, index) => (
+            <Fragment key={notification.id}>
+              <DropdownMenuItem
+                className={`min-w-[300px] max-w-[450px] cursor-pointer focus:text-white ${notification.isRead ? 'opacity-35' : ''}`}
+                onSelect={(event) => {
+                  event.preventDefault()
+                  const notIndex = notifications.findIndex(
+                    (item) => item.id === notification.id
+                  )
+                  if (notIndex !== -1) {
+                    const newNotifications = [...notifications]
+                    newNotifications[notIndex] = {
+                      ...newNotifications[notIndex],
+                      isRead: true,
+                    }
+                    setNotifications(newNotifications)
+                  }
+                }}
+              >
+                <span>{notification.message}</span>
+                <DropdownMenuShortcut>
+                  {notification.timeAgo}
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+              {index < notifications.length - 1 && <DropdownMenuSeparator />}
+            </Fragment>
+          ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
