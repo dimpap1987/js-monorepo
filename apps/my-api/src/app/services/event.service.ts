7c45d2e19b20db14a43a1092e30dc0d3c450ad34
PAPOUTSIS Dimitris <dimitris.papoutsis@sword-group.com>
2024-05-05 23:25:30 +0300

    Notifications: create global channel

--- apps/my-api/src/app/services/event.service.ts
+++ apps/my-api/src/app/services/event.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common'
-import { fromEvent } from 'rxjs'
+import { Observable, fromEvent, merge } from 'rxjs'
 import { EventEmitter } from 'events'
 
 @Injectable()
 export class EventsService {
   private readonly emitter = new EventEmitter()
 
-  subscribe(channel: string) {
-    return fromEvent(this.emitter, channel)
+  subscribe(channel: string | string[]): Observable<any> {
+    if (Array.isArray(channel)) {
+      // If multiple channels are provided, create an observable for each channel
+      const observables = channel?.map((chan) => fromEvent(this.emitter, chan))
+      // Merge the observables into a single observable stream
+      return merge(...observables)
+    } else {
+      // If a single channel is provided, create an observable for that channel
+      return fromEvent(this.emitter, channel)
+    }
   }
 
   emit(channel: string, data?: object) {
