c225fad4aba40dc39e6310f21c1fe20191299b0d
PAPOUTSIS Dimitris <dimitris.papoutsis@sword-group.com>
2024-05-05 21:09:55 +0300

    Rename nestjs app to 'my-api'

--- apps/my-api/jest.config.ts
+++ apps/my-api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'auth-server',
+  displayName: 'my-api',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/auth-server',
+  coverageDirectory: '../../coverage/apps/my-api',
 }
