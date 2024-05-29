931937171f559a7905d88655d815ca7424987c2e
PAPOUTSIS Dimitris <dimitris.papoutsis@sword-group.com>
2024-05-29 21:19:48 +0300

    Add admin announcement form

---
+++ libs/shared/ui/components/src/lib/textarea/textarea-form.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Button } from '../button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../form'
+import { Textarea } from './textarea'
+
+const FormSchema = z.object({
+  notification: z
+    .string()
+    .min(1, {
+      message: 'Notification must be at least 10 characters.',
+    })
+    .max(160, {
+      message: 'Notification must not be longer than 30 characters.',
+    }),
+})
+
+//TODO REFACTOR THIS COMPONENT - MAKE IT REUSABLE
+export function TextareaForm({
+  submitCallBack,
+}: {
+  submitCallBack: (data: z.infer<typeof FormSchema>) => any
+}) {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    return submitCallBack(data)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-3">
+        <FormField
+          control={form.control}
+          name="notification"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Notification</FormLabel>
+              <FormControl>
+                <Textarea className="resize-none" {...field} />
+              </FormControl>
+              <FormDescription>
+                Send this notification directly to the user
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Send</Button>
+      </form>
+    </Form>
+  )
+}
