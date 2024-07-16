f79eced8b5ea72647ce7d27f5e061a73b957354f
PAPOUTSIS Dimitris <dimitris.papoutsis@sword-group.com>
2024-07-16 16:16:08 +0300

    chore: github actions changes

--- apps/next-app/app/(main)/page.tsx
+++ apps/next-app/app/(main)/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next'
 import LandingComponent from '../../components/landing.component'
 
 export const metadata: Metadata = {
-  title: 'My App 😎',
+  title: 'My Super App 😎',
 }
 
 export default function LandingPage() {
