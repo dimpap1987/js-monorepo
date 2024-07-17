68ade7acd40546a4eea893ef8a5aa05bc09a8bb0
PAPOUTSIS Dimitris <dimitris.papoutsis@sword-group.com>
2024-07-18 01:44:58 +0300

    more testing - 5

--- libs/prisma/db/src/lib/db-client.ts
+++ libs/prisma/db/src/lib/db-client.ts
@@ -1,14 +1,13 @@
-import {
-  INestApplication,
-  Injectable,
-  Logger,
-  OnModuleInit,
-} from '@nestjs/common'
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger(PrismaService.name)
+
+  private readonly maxRetries = 10
+
+  private readonly retryDelay = 10000 // 10 seconds
 
   constructor() {
     super({
@@ -23,11 +22,33 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   }
 
   async onModuleInit() {
-    await this.$connect()
-    this.logger.log('Connected to database')
+    let retryCount = 0
 
-    this.$on('query' as never, (e) => {
-      this.logger.log(e)
-    })
+    while (retryCount < this.maxRetries) {
+      try {
+        await this.$connect()
+        this.logger.log('Connected to database')
+        return
+      } catch (error) {
+        retryCount++
+        this.logger.error(
+          `Error connecting to database (attempt ${retryCount}/${this.maxRetries})`,
+          error
+        )
+
+        if (retryCount < this.maxRetries) {
+          this.logger.log(
+            `Retrying connection in ${this.retryDelay / 1000} seconds...`
+          )
+          // eslint-disable-next-line @typescript-eslint/no-loop-func
+          await new Promise((resolve) => setTimeout(resolve, this.retryDelay))
+        }
+      }
+    }
+
+    this.logger.error(
+      'Maximum number of retries reached. Unable to connect to the database.'
+    )
+    throw new Error('Failed to connect to the database')
   }
 }
