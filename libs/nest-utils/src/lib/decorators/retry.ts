3a18d7300f40082fa3580f4fe6bb50e93ba06a65
PAPOUTSIS Dimitris <dimitris.papoutsis@sword-group.com>
2024-07-26 17:05:14 +0300

    fix: Retry decorator

--- libs/nest-utils/src/lib/decorators/retry.ts
+++ libs/nest-utils/src/lib/decorators/retry.ts
@@ -1,6 +1,6 @@
 import { Logger } from '@nestjs/common'
 
-const LOGGER = new Logger('Retry')
+// const LOGGER = new Logger('Retry')
 
 export function Retry(retries: number, timeout = 2000) {
   return function (
@@ -9,22 +9,27 @@ export function Retry(retries: number, timeout = 2000) {
     descriptor: PropertyDescriptor
   ) {
     const originalMethod = descriptor.value
-
+    const logger = new Logger(`${target.constructor.name}-Retry`);
     let originalError: unknown = null
 
     descriptor.value = async function (...args: any[]) {
-      for (let i = 0; i < retries; i++) {
+      for (let i = 0; i <= retries; i++) {
         try {
-          await new Promise((resolve) => setTimeout(resolve, timeout))
           return await originalMethod.apply(this, args)
         } catch (error) {
+          originalError = error
           const methodName = propertyKey
           const className = target.constructor.name
-          LOGGER.error(
-            `Retry - Attempt ${i + 1} - ${className} - ${methodName}`
-          )
-          originalError = error
-          timeout *= 2
+          if (i == 0) {
+            logger.error(`Retry - ${className} - ${methodName}`)
+          } else {
+            logger.error(`Retry - Attempt ${i} - ${className} - ${methodName}`)
+          }
+          if (i < retries) {
+            // dont wait on the last retry
+            await new Promise((resolve) => setTimeout(resolve, timeout))
+            timeout *= 2
+          }
         }
       }
       if (originalError) throw originalError
