6fe1f1b14eb607fe56012ab0662f3e483326a6da
PAPOUTSIS Dimitris <dimitris.papoutsis@sword-group.com>
2024-07-26 03:06:16 +0300

    feature: logger service

--- libs/nest-utils/src/index.ts
+++ libs/nest-utils/src/index.ts
@@ -1,3 +1,1 @@
-export * from './lib/middlewares/compose-middlewares'
-export * from './lib/middlewares/withCrf'
-export * from './lib/middlewares/withPathName'
+export * from './lib/logger/logger.service'
