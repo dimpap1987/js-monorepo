1e8ad966e303fefdf703b39a0e8e6f71193459b1
dpapoutsis <dimitris.papoutsis@sword-group.com>
2024-09-03 15:18:26 +0300

    avatar component

---
+++ libs/shared/ui/components/src/lib/avatar/avatar.tsx
@@ -0,0 +1,59 @@
+import * as AvatarPrimitive from '@radix-ui/react-avatar'
+import * as React from 'react'
+
+import { cn } from '@js-monorepo/ui/util'
+import Image from 'next/image'
+
+const Avatar = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cn(
+      'relative flex h-12 w-12 shrink-0 overflow-hidden rounded-full',
+      className
+    )}
+    {...props}
+  />
+))
+Avatar.displayName = AvatarPrimitive.Root.displayName
+
+const AvatarImage = React.forwardRef<
+  React.ElementRef<typeof Image>,
+  React.ComponentPropsWithoutRef<typeof Image>
+>(({ className, ...props }, ref) => {
+  const [hasError, setHasError] = React.useState(false)
+
+  if (hasError) return null
+
+  return (
+    <Image
+      layout="fill"
+      objectFit="contain"
+      ref={ref}
+      onError={() => setHasError(true)}
+      className={cn('aspect-square', className)}
+      {...props}
+    />
+  )
+})
+
+AvatarImage.displayName = Image.displayName
+
+const AvatarFallback = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Fallback>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cn(
+      'flex h-full w-full items-center justify-center rounded-full bg-muted',
+      className
+    )}
+    {...props}
+  />
+))
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
+
+export { Avatar, AvatarFallback, AvatarImage }
