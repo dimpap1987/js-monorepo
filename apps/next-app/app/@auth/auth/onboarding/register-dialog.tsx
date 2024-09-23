53e5c2c3cdb759882c34aa3cdc2574339edd977a
dpapoutsis <dimitris.papoutsis@sword-group.com>
2024-09-23 14:59:28 +0300

    README updates

--- apps/next-app/app/@auth/auth/onboarding/register-dialog.tsx
+++ apps/next-app/app/@auth/auth/onboarding/register-dialog.tsx
@@ -1,7 +1,11 @@
 'use client'
 
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '@js-monorepo/components/avatar'
 import { RegisterDialogComponent } from '@js-monorepo/dialog'
-import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { RegisterForm } from './register-form'
@@ -23,17 +27,20 @@ export function RegisterDialog({
 
   return (
     <RegisterDialogComponent open={mounted}>
-      {userProfileImage && (
-        <div className="p-2 flex justify-center">
-          <Image
-            className="rounded-2xl"
-            width={80}
-            height={80}
-            alt="user profile"
-            src={userProfileImage}
-          ></Image>
-        </div>
-      )}
+      <div className="p-2 flex justify-center">
+        <Avatar className="h-20 w-20">
+          {userProfileImage && (
+            <AvatarImage
+              src={userProfileImage}
+              alt={`user's picture`}
+            ></AvatarImage>
+          )}
+          <AvatarFallback>
+            {formInput?.email?.slice(0, 2)?.toUpperCase() || 'A'}
+          </AvatarFallback>
+        </Avatar>
+      </div>
+
       <RegisterForm formInput={formInput}></RegisterForm>
     </RegisterDialogComponent>
   )
