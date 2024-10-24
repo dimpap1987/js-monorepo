3e43768d08568e7021bb2e7a3326038537d2d5b5
dpapoutsis <dimitris.papoutsis@sword-group.com>
2024-10-24 19:21:29 +0300

    buttons variants changes

--- libs/shared/ui/button/src/lib/button-variants.tsx
+++ libs/shared/ui/button/src/lib/button-variants.tsx
@@ -3,15 +3,15 @@ export const buttonVariants = {
   variants: {
     variant: {
       primary:
-        'bg-primary border border-primary/60 text-primary-foreground hover:bg-primary/80 focus:bg-primary/80 [text-shadow:1px_0px_1px_hsl(var(--tw-shadow-color))]',
+        'bg-primary border-2 border-primary/80 text-primary-foreground hover:bg-primary/60 focus:bg-primary/60 [text-shadow:1px_0px_1px_hsl(var(--tw-shadow-color))]',
       accent:
-        'bg-accent border border-accent/60 text-accent-foreground hover:bg-accent/80 focus:bg-accent/80 [text-shadow:1px_0px_1px_hsl(var(--tw-shadow-color))]',
+        'bg-accent border-2 border-accent/80 text-accent-foreground hover:bg-accent/60 focus:bg-accent/60 [text-shadow:1px_0px_1px_hsl(var(--tw-shadow-color))]',
       secondary:
-        'bg-secondary border border-secondary/60 text-secondary-foreground hover:bg-secondary/80 focus:bg-secondary/80 [text-shadow:1px_0px_1px_hsl(var(--tw-shadow-color))]',
+        'bg-secondary border-2 border-secondary/80 text-secondary-foreground hover:bg-secondary/60 focus:bg-secondary/60 [text-shadow:1px_0px_1px_hsl(var(--tw-shadow-color))]',
       danger:
-        'bg-danger border border-danger/60 text-danger-foreground hover:bg-danger/80 focus:bg-danger/80 [text-shadow:1px_0px_1px_hsl(var(--tw-shadow-color))]',
+        'bg-danger border-2 border-danger/80 text-danger-foreground hover:bg-danger/60 focus:bg-danger/60 [text-shadow:1px_0px_1px_hsl(var(--tw-shadow-color))]',
       outline:
-        'border border-input bg-background hover:bg-accent text-foreground hover:text-primary-foreground',
+        'border-2 border-primary/80 bg-background hover:bg-accent text-foreground hover:text-primary-foreground',
     },
     size: {
       default: 'h-8 px-4 py-2',
