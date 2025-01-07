ded71bc9a5bd1c40e74f55e0ae294140ca41d4b8
dpapoutsis <dimitris.papoutsis@sword-group.com>
2025-01-07 16:42:29 +0200

    payments subscriptions changes

--- libs/payments/client/src/lib/components/pricing.tsx
+++ libs/payments/client/src/lib/components/pricing.tsx
@@ -1,56 +1,34 @@
 'use client'
 
-import { useRouter } from 'next-nprogress-bar'
 import { useSession } from '@js-monorepo/auth/next/client'
-import { DpButton } from '@js-monorepo/button'
-import { Tabs, TabsList, TabsTrigger } from '@js-monorepo/components/tabs'
 import { PricingPlanResponse } from '@js-monorepo/types'
-import { cn } from '@js-monorepo/ui/util'
 import { loadStripe } from '@stripe/stripe-js'
+import { useRouter } from 'next-nprogress-bar'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { apiCheckoutPlan, apiGetPlans } from '../utils/api'
+import { PlanCard, PlanCardProps } from './plan-card'
 
-type PricingCardProps = {
-  handleCheckout?: () => Promise<any>
-  price: number
-  title: string
-  description: string
-  features: Record<string, string>
-  interval: string
-  actionLabel?: string
-  popular?: boolean
-  subscribed?: boolean
-  isLoggedIn?: boolean
+interface Product {
+  id: number
+}
+interface Price {
+  product: Product
+}
+interface Plan {
+  price?: Price
+}
+interface Subscription {
+  plans?: Plan[]
 }
 
-type PlansInterval = 'month' | 'year'
-
-type PricingCardWithPriceId = Omit<
-  { id: number } & PricingCardProps,
+type PlanCardPropsWithId = Omit<
+  { id: number } & PlanCardProps,
   'isLoggedIn' | 'handleCheckout'
 >
 
-type PricingSwitchProps = {
-  onSwitch: (value: PlansInterval) => void
-}
-
-const CheckItem = ({ text }: { text: string }) => (
-  <>
-    <svg
-      className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-      fill="currentColor"
-      viewBox="0 0 20 20"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        fillRule="evenodd"
-        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-        clipRule="evenodd"
-      ></path>
-    </svg>
-    <span>{text}</span>
-  </>
-)
+const isSubscribed = (subscription: Subscription, planId: number) =>
+  subscription?.plans?.some((p: Plan) => p.price?.product?.id === planId) ||
+  false
 
 const PricingHeader = ({ title }: { title: string }) => (
   <section className="text-center">
@@ -61,132 +39,22 @@ const PricingHeader = ({ title }: { title: string }) => (
   </section>
 )
 
-const PricingSwitch = ({ onSwitch }: PricingSwitchProps) => (
-  <Tabs
-    defaultValue="month"
-    className="max-w-max mx-auto dark:bg-gray-800 p-2 mt-2 rounded-lg"
-    onValueChange={(value) => onSwitch(value as PlansInterval)}
-  >
-    <TabsList className="p1">
-      <TabsTrigger value="month" className="font-semibold text-lg" role="tab">
-        <p>Monthly</p>
-      </TabsTrigger>
-      <TabsTrigger value="year" className="font-semibold text-lg" role="tab">
-        <p>Yearly</p>
-      </TabsTrigger>
-    </TabsList>
-  </Tabs>
-)
-
-const PricingCard = ({
-  handleCheckout,
-  title,
-  price,
-  interval,
-  description,
-  features,
-  actionLabel,
-  subscribed,
-  isLoggedIn,
-  anySubscribed,
-}: PricingCardProps & { anySubscribed: boolean }) => {
-  const [isLoading, setIsLoading] = useState(false)
-  const router = useRouter()
-
-  const isFree = price === 0
-
-  const onCheckout = async () => {
-    setIsLoading(true)
-    try {
-      if (!isLoggedIn) {
-        router.push('/auth/login')
-        return
-      }
-      await handleCheckout?.()
-    } catch (error) {
-      console.error('Stripe Checkout Error:', error)
-    } finally {
-      setIsLoading(false)
-    }
-  }
-
-  return (
-    <div
-      className={cn(
-        'relative w-full max-w-[360px] sm:w-[360px] shadow-lg flex mx-auto rounded-xl border-2 border-border transform transition-transform duration-300',
-        anySubscribed && !subscribed && 'grayscale-[50%] opacity-55',
-        subscribed && 'glow',
-        'hover:opacity-100'
-      )}
-    >
-      <div className="content bg-background-card p-6 rounded-xl flex flex-col justify-around text-center w-full">
-        {subscribed && (
-          <div
-            className="bg-green-50 text-green-600 absolute top-0 
-                    -translate-y-1/2 right-5 z-10 p-1.5 px-3 border border-green-400 
-                    rounded-full shadow-md font-medium"
-          >
-            Active
-          </div>
-        )}
-
-        <h3 className="mb-4 text-2xl font-semibold">{title}</h3>
-        <p className="font-light text-foreground-neutral sm:text-lg">
-          {description}
-        </p>
-        <div className="flex justify-center items-baseline my-6">
-          <span className="mr-2 text-5xl font-extrabold text-gray-900 dark:text-white">
-            {`$${price}`}
-          </span>
-
-          <span className="text-gray-500 dark:text-gray-400">
-            {interval && `/${interval}`}
-          </span>
-        </div>
-        <ul role="list" className="mb-8 space-y-3 text-left mt-2">
-          {Object.entries(features).map(([key, value]) => (
-            <li key={key} className="flex items-center space-x-3">
-              <CheckItem text={value} />
-            </li>
-          ))}
-        </ul>
-        <DpButton
-          size="large"
-          loading={isLoading}
-          onClick={onCheckout}
-          disabled={isLoggedIn && (subscribed || isFree)}
-        >
-          {actionLabel}
-        </DpButton>
-      </div>
-    </div>
-  )
-}
-
 export function Pricing() {
-  const [interval, setInterval] = useState<PlansInterval>('month')
   const [plans, setPlans] = useState<PricingPlanResponse[]>([])
   const {
     session: { subscription },
     isLoggedIn,
   } = useSession()
+  const router = useRouter()
   const stripePromise = useMemo(
     () => loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!),
     []
   )
 
-  const isSubscribed = (priceId: number) =>
-    subscription?.plans?.some((p: { priceId: number }) => p.priceId === priceId)
-
-  const anySubscribed = useMemo(
-    () => subscription?.plans?.length > 0,
-    [subscription]
-  )
-
-  const pricingCards: PricingCardWithPriceId[] = plans
+  const pricingCards: PlanCardPropsWithId[] = plans
     .flatMap((plan) =>
       plan.prices
-        .filter((price) => price.interval === interval)
+        .filter((price) => price.interval === 'month')
         .map((price) => ({
           id: price.id,
           title: plan.title,
@@ -194,9 +62,8 @@ export function Pricing() {
           price: price.unitAmount / 100,
           interval: price.interval,
           features: plan.features,
-          actionLabel: plan.features['label'] || 'Get Started',
-          subscribed: isSubscribed(price.id),
-          isLoggedIn: isLoggedIn,
+          actionLabel: 'Get Started',
+          subscribed: isSubscribed(subscription, plan.id),
         }))
     )
     .sort((a, b) => a.price - b.price)
@@ -216,6 +83,10 @@ export function Pricing() {
   const handleCheckout = useCallback(
     async (priceId: number) => {
       try {
+        if (!isLoggedIn) {
+          router.push('/auth/login')
+          return
+        }
         const response = await apiCheckoutPlan(priceId)
         if (response.ok) {
           const stripe = await stripePromise
@@ -239,21 +110,14 @@ export function Pricing() {
   return (
     <div>
       <PricingHeader title="Select a Pricing Plan" />
-      <PricingSwitch onSwitch={setInterval} />
       <section className="grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(340px,1fr))] gap-6 mt-10">
         {pricingCards?.map((card) => (
-          <PricingCard
+          <PlanCard
             key={card.id}
-            description={card.description}
-            features={card.features}
             handleCheckout={() => handleCheckout(card.id)}
-            price={card.price}
-            title={card.title}
-            actionLabel={card.actionLabel}
-            subscribed={card.subscribed}
-            interval={card.interval}
+            anySubscribed={subscription?.plans?.length > 0}
             isLoggedIn={isLoggedIn}
-            anySubscribed={anySubscribed}
+            {...card}
           />
         ))}
       </section>
--- libs/prisma/db/src/lib/prisma/migrations/20250106000052_insert_payments_products/migration.sql
+++ libs/prisma/db/src/lib/prisma/migrations/20250106000052_insert_payments_products/migration.sql
@@ -1,14 +1,11 @@
 INSERT INTO "products" (id, "stripeId", name, description, active, features, "updatedAt")
 VALUES
-  (1, 'prod_RQNmZkTFUlPKEXH', 'Free Plan', 'Free features', true, '{"key1":"feature 1"}', now()),
+  (1, 'prod_RXfHdROfnLBsKo', 'Free Plan', 'Free features', true, '{"key1":"feature 1"}', now()),
   (2, 'prod_RQDnAmArtPERmBh', 'Basic Plan', 'The basic plan to start', true, '{"key1":"feature 1","key2":"feature 2"}', now()),
   (3, 'prod_RQJlgfOPUlPKEH', 'Pro Plan', 'All features unlocked', true, '{"key1":"feature 1","key2":"feature 2","key3":"feature 3"}', now());
 
 INSERT INTO "prices" ("stripeId", "unitAmount", "currency", "interval", "productId", "updatedAt")
 VALUES
+  ('price_1QeZwgFhc1HIVKp4HPYAWB74', 0, 'EUR', 'month', 1, now()),
   ('price_1QXNM6Fhc1HIVKp4r9eyOT9k', 1000, 'EUR', 'month', 2, now()),
-  ('price_2QXT6XFhc1HIVKp4Jkaj7I0E', 10000, 'EUR', 'year', 2, now()),
-  ('price_1QXT7yFhc1HIVKp4Hmv6TeoC', 2500, 'EUR', 'month', 3, now()),
-  ('price_1QXT8TFhc1HIVKp4EV8n4CX0', 20000, 'EUR', 'year', 3, now()),
-  ('free_month', 0, 'EUR', 'month', 1, now()),
-  ('free_year', 0, 'EUR', 'year', 1, now());
+  ('price_1QXT7yFhc1HIVKp4Hmv6TeoC', 2500, 'EUR', 'month', 3, now());
