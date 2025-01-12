73aafdb3fa7207f628f02577e68d0d565d04ec5f
dpapoutsis <dimitris.papoutsis@sword-group.com>
2025-01-12 15:18:32 +0200

    remove buttons overflow

--- libs/shared/ui/button/src/lib/button-variants.tsx
+++ libs/shared/ui/button/src/lib/button-variants.tsx
@@ -1,5 +1,5 @@
 export const buttonVariants = {
-  base: 'relative text-base rounded font-semibold flex items-center justify-center whitespace-nowrap disabled:opacity-50 disabled:cursor-not-allowed transition-colors duration-500 ease-in-out overflow-hidden',
+  base: 'relative text-base rounded font-semibold flex items-center justify-center whitespace-nowrap disabled:opacity-50 disabled:cursor-not-allowed transition-colors duration-500 ease-in-out',
   variants: {
     variant: {
       primary:
