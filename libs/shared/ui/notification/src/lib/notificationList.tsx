32459bbee479cb284525cd1402e11cb236d49d99
dpapoutsis <dimitris.papoutsis@sword-group.com>
2025-01-30 15:19:01 +0200

    notification component css changes - format

--- libs/shared/ui/notification/src/lib/notificationList.tsx
+++ libs/shared/ui/notification/src/lib/notificationList.tsx
@@ -13,10 +13,7 @@ export default function DpNotificationList({ notifications, overlayClassName }: 
     notifications?.length > 0 && (
       <section className={cn(`fixed top-navbar-offset right-2 p-2 z-40 flex flex-col-reverse gap-2`, overlayClassName)}>
         {notifications.map((notification, index) => (
-          <div
-            key={notification.id || index}
-            className={`${styles.notificationContainer} flex bg-background-secondary p-2 border-purple-500 border-2 rounded-md text-sm text-white`}
-          >
+          <div key={notification.id || index} className={`${styles.notificationContainer} flex text-sm text-white`}>
             <Notification {...notification} />
           </div>
         ))}
