1c23231f4ecda3f555cdd5d6e5e302ce36d8d917
Dp <mits0s200efta@gmail.com>
2025-12-18 18:24:13 +0200

    chore: add jest test setup for sidebar component
    - Add jest.config.ts for sidebar component
    - Add test-setup files for jest configuration
    - Add tsconfig.spec.json for test TypeScript config
    - Update package.json and package-lock.json dependencies
    - Update root jest.config.ts
    - Add jest setup scripts and templates

---
+++ libs/shared/ui/sidebar/jest.config.ts
@@ -0,0 +1,25 @@
+/* eslint-disable */
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('../../../../tsconfig.base.json')
+
+module.exports = {
+  displayName: 'sidebar',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: [['@babel/preset-react', { runtime: 'automatic' }]] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/shared/ui/sidebar',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths || {}, {
+      prefix: '<rootDir>/../../../../',
+    }),
+  },
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.d.ts', '!**/*.stories.{ts,tsx}', '!**/index.{ts,tsx}'],
+  clearMocks: true,
+  restoreMocks: true,
+}
