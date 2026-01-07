8776cdfa6b83d2f383124234fd6002f8852825b4
Dp <mits0s200efta@gmail.com>
2026-01-07 20:07:48 +0200

    style: Refactor global styles for consistent sizing

--- libs/shared/ui/templates/src/lib/body.tsx
+++ libs/shared/ui/templates/src/lib/body.tsx
@@ -2,9 +2,5 @@ import { cn } from '@js-monorepo/ui/util'
 import { PropsWithChildren } from 'react'
 
 export function BodyTemplate({ children, className }: PropsWithChildren & { className?: string }) {
-  return (
-    <body className={cn('flex flex-col min-h-100svh min-w-[200px]', 'overflow-x-hidden w-[100vw]', className)}>
-      {children}
-    </body>
-  )
+  return <body className={cn('flex flex-col min-w-[200px]', className)}>{children}</body>
 }
