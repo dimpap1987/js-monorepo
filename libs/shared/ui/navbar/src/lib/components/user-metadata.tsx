e5c08a69f3c52df5005d04cc185c919810d5ca00
Dp <mits0s200efta@gmail.com>
2026-01-07 20:08:11 +0200

    style: Apply minor UI adjustments to various components

--- libs/shared/ui/navbar/src/lib/components/user-metadata.tsx
+++ libs/shared/ui/navbar/src/lib/components/user-metadata.tsx
@@ -17,16 +17,18 @@ export function UserMetadata({
   className?: string | null
 }) {
   return (
-    <div className={cn('px-1 py-3 flex gap-3 items-center', className)}>
+    <div className={cn('px-1 py-4 flex gap-3 items-center w-full', className)}>
       <Avatar className="h-10 w-10">
         {profileImage && <AvatarImage src={profileImage} alt={`${username} picture`}></AvatarImage>}
         <AvatarFallback className="text-sm font-semibold">{username?.slice(0, 2)?.toUpperCase() || 'A'}</AvatarFallback>
       </Avatar>
 
       <div className="flex-1 min-w-0">
-        <div className="font-semibold flex items-center gap-1.5 text-sm">
-          <FaAt className="text-foreground-muted flex-shrink-0" />
-          <span className="truncate">{username}</span>
+        <div className="font-semibold flex items-center gap-1.5 text-sm justify-between">
+          <span className="truncate flex gap-1 items-center">
+            <FaAt className="text-foreground-muted flex-shrink-0" />
+            {username}
+          </span>
           <PlanBadge className="ml-1" plan={plan} />
         </div>
         {createdAt && (
